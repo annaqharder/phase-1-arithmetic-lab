aa67e7b9736e0a7c38d0bd7c283ee3a42b6a0288
Anna Harder <annaqharder@gmail.com>
2022-10-30 14:54:55 -0600

    commit tree 6a691980ddb7f86a8da89d000887833dbd370f47

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+let num1 = 31; 
+let num2 = 2; 
+const multiply = num1 * num2; 
 
-// Write your code here
+
+
+const random = Math.ceil(Math.random()); 
+
+
+let num3 = 16;
+let num4 = 6; 
+const mod = num3 % num4; 
+
+
+const max = Math.max(12, 4, 5, 20, 17); 
+
+
